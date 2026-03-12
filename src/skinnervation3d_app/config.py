d15013e77d7321b67973dab2d81c083b0a4d6e4d
girochat <117285316+girochat@users.noreply.github.com>
2026-03-12 21:41:09 +0100

    REmoved tracking of config file

--- src/skinnervation3d_app/config.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-
-CONDA_NAPARI_ENV_NAME = "napari-crop"
-CONDA_NAPARI_ENV_ROOT = Path(Path.home(), "miniforge3", "envs", CONDA_NAPARI_ENV_NAME)
-#ANALYSIS_DIR_INIT = Path(Path.home() / "Documents/SkInnervationProject/Data/Multitile")
-ANALYSIS_DIR_INIT = Path(Path.home() / "Documents/Data")
